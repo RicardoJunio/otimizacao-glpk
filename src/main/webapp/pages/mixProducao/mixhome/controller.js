285ef9854bf3dd5ebdebf9dbf084fd838a66c332
Ricardo Junio <ricardo.junior.s@hotmail.com>
2017-12-06 18:05:33 -0200

    lágrimas finais

--- src/main/webapp/pages/mixProducao/mixhome/controller.js
+++ src/main/webapp/pages/mixProducao/mixhome/controller.js
@@ -17,18 +17,35 @@ angular.module('Mixproducao')
 
         $scope.submit = function(){
 
-              var obj= [{nome:"Marcelo",idade:20},{nome:"Teste",idade:90}];
+              var api = "https://glpkback.azurewebsites.net";
+              var obj= [
+              {Produto:$scope.x.nome,
+               Valor: $scope.x.valor,
+               Material01: $scope.m1.material,
+               prod1:$scope.m1.prod1,
+               prod2: $scope.m1.prod2,
+               disponibilidade: $scope.m1.disponibilidade,
+               }
+               {Produto:$scope.y.nome,
+               Valor: $scope.y.valor,
+               Material01: $scope.m2.material,
+               prod1:$scope.m2.prod1,
+               prod2: $scope.m2.prod2,
+               disponibilidade: $scope.m2.disponibilidade,
+               }];
+
               var json =JSON.stringify(obj);
 
-              $http.post('/api/authenticate', json)
+              $http.post( api +'/GLPK', json)
                           .success(function (response) {
                           callback(response);
                           });
                if (response.success){
                var x = response.data;
-
+               //jogar na tela aqui
                }
                else{
+               $scope.error = "Aconteceu algum problema com o processamento, tente mais uma vez";
                }
 
         };
